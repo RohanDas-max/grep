0897047b0dea328a02117ca8c2a8ae695102fef3
rohandas-max <rohandasd0@gmail.com>
2022-02-09 19:15:29 +0530

    printing all the values in main

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -5,15 +5,17 @@ import (
 	"log"
 
 	"github.com/rohandas-max/grep/pkg/controller"
+	"github.com/rohandas-max/grep/pkg/utils"
 )
 
 func main() {
 	i := flag.Bool("i", false, "option to search in case-insensitive manner")
 	flag.Parse()
 	args := flag.Args()
-	err := controller.Controller(args, *i)
+	res, err := controller.Controller(args, *i)
 	if err != nil {
 		log.Fatal(err)
 	}
+	utils.PrintLine(res)
 
 }
