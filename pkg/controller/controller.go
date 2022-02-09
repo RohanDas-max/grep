1f5a7c8313f434a0abea0b6cd44d229496918ca3
rohandas-max <rohandasd0@gmail.com>
2022-02-09 15:22:38 +0530

    case insensitive search implemented

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -3,17 +3,22 @@ package controller
 import (
 	"errors"
 	"os"
+	"strings"
 
 	"github.com/rohandas-max/grep/pkg/handler"
 	"github.com/rohandas-max/grep/pkg/utils"
 )
 
-func Controller(args []string) error {
+func Controller(args []string, i bool) error {
+	a := string(args[0])
+	if i {
+		a = strings.ToLower(args[0])
+	}
 	switch {
 	case len(args) < 1:
 		return errors.New("please add some option or command")
 	case len(args) <= 1:
-		if err := handler.SearchInStdin(string(args[0])); err != nil {
+		if err := handler.SearchInStdin(a); err != nil {
 			return err
 		}
 	case len(args) <= 2:
@@ -22,13 +27,13 @@ func Controller(args []string) error {
 			return err
 		}
 		if fs.IsDir() {
-			data, err := handler.SearchInDir(args[1], args[0])
+			data, err := handler.SearchInDir(args[1], a)
 			if err != nil {
 				return errors.New("invalid folder name")
 			}
 			utils.PrintLine(data)
 		} else if !fs.IsDir() {
-			data, err := handler.SearchInAFile(string(args[1]), string(args[0]))
+			data, err := handler.SearchInAFile(string(args[1]), a)
 			if err != nil {
 				return errors.New("invalid file name")
 
--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestController(t *testing.T) {
 	type args struct {
 		args []string
+		i    bool
 	}
 	tests := []struct {
 		name    string
@@ -15,6 +16,7 @@ func TestController(t *testing.T) {
 			name: "pass1",
 			args: args{
 				args: []string{"hello", "../../dir"},
+				i:    false,
 			},
 			wantErr: false,
 		},
@@ -22,6 +24,7 @@ func TestController(t *testing.T) {
 			name: "pass2",
 			args: args{
 				args: []string{"hello", "../../test.txt"},
+				i:    false,
 			},
 			wantErr: false,
 		},
@@ -29,6 +32,7 @@ func TestController(t *testing.T) {
 			name: "pass3",
 			args: args{
 				args: []string{"hello"},
+				i:    false,
 			},
 			wantErr: false,
 		},
@@ -36,6 +40,7 @@ func TestController(t *testing.T) {
 			name: "fail1",
 			args: args{
 				args: []string{"hello", "dir"},
+				i:    false,
 			},
 			wantErr: true,
 		},
@@ -43,6 +48,7 @@ func TestController(t *testing.T) {
 			name: "fail2",
 			args: args{
 				args: []string{"hello", "any", "any"},
+				i:    false,
 			},
 			wantErr: true,
 		},
@@ -50,13 +56,30 @@ func TestController(t *testing.T) {
 			name: "fail3",
 			args: args{
 				args: []string{""},
+				i:    false,
 			},
 			wantErr: true,
+		},
+		{
+			name: "case insensitive1",
+			args: args{
+				args: []string{"Hello", "../../dir"},
+				i:    true,
+			},
+			wantErr: false,
+		},
+		{
+			name: "case insensitive2",
+			args: args{
+				args: []string{"HeLLo", "../../test1.txt"},
+				i:    true,
+			},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := Controller(tt.args.args); (err != nil) != tt.wantErr {
+			if err := Controller(tt.args.args, tt.args.i); (err != nil) != tt.wantErr {
 				t.Errorf("Controller() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
