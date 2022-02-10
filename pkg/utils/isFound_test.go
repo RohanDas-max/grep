4870d7eb7cf4d3dc0c091db1d6d4dec02db5a91d
rohandas-max <rohandasd0@gmail.com>
2022-02-10 13:18:09 +0530

    tests added

---
+++ pkg/utils/isFound_test.go
@@ -0,0 +1,45 @@
+package utils
+
+import "testing"
+
+func TestIsFound(t *testing.T) {
+	type args struct {
+		txt string
+		a   string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 bool
+	}{
+		{
+			name: "",
+			args: args{
+				txt: "Hello World",
+				a:   "hello",
+			},
+			want:  "",
+			want1: false,
+		}, {
+			name: "",
+			args: args{
+				txt: "Hello World",
+				a:   "Hello",
+			},
+			want:  "Hello World",
+			want1: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := IsFound(tt.args.txt, tt.args.a)
+			if got != tt.want {
+				t.Errorf("IsFound() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("IsFound() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
