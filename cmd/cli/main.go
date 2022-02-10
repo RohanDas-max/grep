c93169baeb2bf657eb7015633eff25085eb5cb08
rohandas-max <rohandasd0@gmail.com>
2022-02-10 22:45:31 +0530

    flags added

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/rohandas-max/grep/pkg/controller"
@@ -10,12 +11,26 @@ import (
 
 func main() {
 	i := flag.Bool("i", false, "option to search in case-insensitive manner")
+	c := flag.Bool("c", false, "option to count")
+	C := flag.Int("C", 0, "")
+	A := flag.Int("A", 0, "")
+	B := flag.Int("B", 0, "")
 	flag.Parse()
 	args := flag.Args()
-	res, err := controller.Controller(args, *i)
+	var ControlFlag = make(map[string]bool)
+	ControlFlag["i"] = *i
+	ControlFlag["c"] = *c
+	var ContextFlag = make(map[string]int)
+	ContextFlag["A"] = *A
+	ContextFlag["B"] = *B
+	ContextFlag["C"] = *C
+
+	res, count, err := controller.Controller(args, ControlFlag, ContextFlag)
 	if err != nil {
 		log.Fatal(err)
+	} else if count > 0 {
+		fmt.Printf("%d\n", count)
+	} else {
+		utils.PrintLine(res)
 	}
-	utils.PrintLine(res)
-
 }
--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -3,33 +3,27 @@ package controller
 import (
 	"errors"
 	"os"
-	"strings"
 
 	"github.com/rohandas-max/grep/pkg/handler"
 )
 
-func Controller(args []string, iFlag bool) ([]string, error) {
-	var res []string
-	a := string(args[0])
-	if iFlag {
-		a = strings.ToLower(args[0])
-	}
-
+func Controller(args []string, c map[string]bool, cf map[string]int) ([]string, int, error) {
+	res := []string{}
 	switch len(args) {
 	case 1:
-		return res, handler.SearchInStdin(a)
+		return res, 0, handler.SearchInStdin(args[0], c, cf)
 	case 2:
 		fs, err := os.Stat(args[1])
 		if err != nil {
-			return res, err
+			return res, 0, err
 		}
 		if fs.IsDir() {
-			return handler.SearchInDir(args[1], a)
+			return handler.SearchInDir(args[1], args[0], c, cf)
 		} else if !fs.IsDir() {
-			return handler.SearchInAFile(string(args[1]), a)
+			return handler.SearchInAFile(string(args[1]), args[0], c, cf)
 		}
 	default:
-		return res, errors.New("oops! wrong command")
+		return res, 0, errors.New("oops! wrong command")
 	}
-	return res, nil
+	return res, 0, nil
 }
