7ee1cdcfd57f58e6307eebd9cc51fc3a3af4d054
rohandas-max <rohandasd0@gmail.com>
2022-02-10 12:45:47 +0530

    search fixed and now works with SearchInStdin() also

--- pkg/utils/search.go
+++ pkg/utils/search.go
@@ -2,16 +2,25 @@ package utils
 
 import "fmt"
 
-func Search(data []string, a, path string, res []string) []string {
-	for _, line := range data {
-		if v, found := IsFound(line, a); found {
-			if path != "" {
-				s := fmt.Sprintf("%v:%v", path, v)
-				res = append(res, s)
-			} else {
-				res = append(res, v)
+func Search(data []string, a, path string, stdin bool) ([]string, string) {
+	var res []string
+	var s string
+	if !stdin {
+		for _, line := range data {
+			if v, found := IsFound(line, a); found {
+				if path != "" {
+					s := fmt.Sprintf("%v:%v", path, v)
+					res = append(res, s)
+				} else {
+					res = append(res, v)
+				}
 			}
 		}
+		return res, s
+	} else if stdin {
+		if v, found := IsFound(data[0], a); found {
+			return res, v
+		}
 	}
-	return res
+	return res, s
 }
